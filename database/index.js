a49c3614889465bd467da80190847b0bcc349156
LachelleF <lachellefryett@gmail.com>
2020-06-22 19:40:14 -0400

    Reviewed changes with PR reviewer. Looking for approval on recommit

--- database/index.js
+++ database/index.js
@@ -13,13 +13,6 @@ db.once('openUri', function () {
   console.log('mongoose connected successfully');
 });
 
-// //Chris' way
-// const db = mongoose.connection;
-// db.on('error', console.error.bind(console, 'connection error:'));
-// db.once('open', function() {
-//   // we're connected!
-// });
-
 const otherPopularGamesSchema = mongoose.Schema({
   genres: String,
   relatedGames: String
@@ -28,3 +21,6 @@ const otherPopularGamesSchema = mongoose.Schema({
 const OtherPopularGames = mongoose.model('OtherPopularGames', otherPopularGamesSchema);
 
 module.exports.db = db;
+module.exports = OtherPopularGames;
+//db methods to be added
+
