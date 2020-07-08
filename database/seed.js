7c4bdd37a668b56d6f13a573355b1194b3d8b9ac
LachelleF <lachellefryett@gmail.com>
2020-07-08 16:06:42 -0400

    Database seeded and tested

--- database/seed.js
+++ database/seed.js
@@ -4,10 +4,99 @@ const db = require('./index.js');
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
+//used to store randomly generated genre arrays of game Ids for each of 16 genres
+//these arrays will be passed to images, systemReqs endpoints to build up carousel of
+//other related games
+let genres = [
+  {
+    genreId: 1,
+    genreName: "Action",
+    genreRelatedGames: []
+  },
+  {
+    id: 2,
+    genreName: "Adventure",
+    genreRelatedGames: []
+  },
+  {
+    id: 3,
+    genreName: "FPS",
+    genreRelatedGames: []
+  },
+  {
+    id: 4,
+    genreName: "Indie",
+    genreRelatedGames: []
+  },
+  {
+    id: 5,
+    genreName: "MMO",
+    genreRelatedGames: []
+  },
+  {
+    id: 6,
+    genreName: "Multiplayer",
+    genreRelatedGames: []
+  },
+  {
+    id: 7,
+    genreName: "Puzzle",
+    genreRelatedGames: []
+  },
+  {
+    id: 8,
+    genreName: "Racing",
+    genreRelatedGames: []
+  },
+  {
+    id: 9,
+    genreName: "Retro",
+    genreRelatedGames: []
+  },
+  {
+    id: 10,
+    genreName: "RPG",
+    genreRelatedGames: []
+  },
+  {
+    id: 11,
+    genreName: "Simulation",
+    genreRelatedGames: []
+  },
+  {
+    id: 12,
+    genreName: "Sports",
+    genreRelatedGames: []
+  },
+  {
+    id: 13,
+    genreName: "Stealth",
+    genreRelatedGames: []
+  },
+  {
+    id: 14,
+    genreName: "Strategy",
+    genreRelatedGames: []
+  },
+  {
+    id: 15,
+    genreName: "Tabletop",
+    genreRelatedGames: []
+  },
+  {
+    id: 16,
+    genreName: "Virtual Reality",
+    genreRelatedGames: []
+  },
+]
+
 // generating 100 records because this is test data
 let seed = function () {
-  for (var i = 0; i <= 100; i++) {
-    genres[randomGameGenreId(1, 16)].product_id_Array.push(i);
+
+  for (var i = 1; i <= 100; i++) {
+    let randomGenreId = randomGameGenreId(0, 15);
+    let randomGameId = i;
+    genres[randomGenreId].genreRelatedGames.push(randomGameId);
   }
   sampleFileGenerator(genres);
   return genres;
@@ -17,101 +106,15 @@ let seed = function () {
 let seedToDB = function () {
   //call seed fn
   let data = seed();
-
   //insert records into db
-  return db.collection.insertMany(data)
-    //response fr db
-    .then(result => {
-      console.log('Successfully inserted items: ', result);
-      return result;
-    })
-    .catch(err => console.error(`Failed to insert documents: ${err}`));
+  // return db.collection.insertMany(data)
+  //   //response fr db
+  //   .then(result => {
+  //     console.log('Successfully inserted items: ', result);
+  //     return result;
+  //   })
+  //   .catch(err => console.error(`Failed to insert documents: ${err}`));
 };
-//used to store randomly generated genre arrays of game Ids for each of 16 genres
-//these arrays will be passed to images, systemReqs endpoints to build up carousel of
-//other related games
-let genres = [
-  {
-    id: 1,
-    name: "Action",
-    product_id_Array: []
-  },
-  {
-    id: 2,
-    name: "Adventure",
-    product_id_Array: []
-  },
-  {
-    id: 3,
-    name: "FPS",
-    product_id_Array: []
-  },
-  {
-    id: 4,
-    name: "Indie",
-    product_id_Array: []
-  },
-  {
-    id: 5,
-    name: "MMO",
-    product_id_Array: []
-  },
-  {
-    id: 6,
-    name: "Multiplayer",
-    product_id_Array: []
-  },
-  {
-    id: 7,
-    name: "Puzzle",
-    product_id_Array: []
-  },
-  {
-    id: 8,
-    name: "Racing",
-    product_id_Array: []
-  },
-  {
-    id: 9,
-    name: "Retro",
-    product_id_Array: []
-  },
-  {
-    id: 10,
-    name: "RPG",
-    product_id_Array: []
-  },
-  {
-    id: 11,
-    name: "Simulation",
-    product_id_Array: []
-  },
-  {
-    id: 12,
-    name: "Sports",
-    product_id_Array: []
-  },
-  {
-    id: 13,
-    name: "Stealth",
-    product_id_Array: []
-  },
-  {
-    id: 14,
-    name: "Strategy",
-    product_id_Array: []
-  },
-  {
-    id: 15,
-    name: "Tabletop",
-    product_id_Array: []
-  },
-  {
-    id: 16,
-    name: "Virtual Reality",
-    product_id_Array: []
-  },
-]
 
 //Returns an integer random number between min (included) and max (included):
 function randomGameGenreId(min, max) {
